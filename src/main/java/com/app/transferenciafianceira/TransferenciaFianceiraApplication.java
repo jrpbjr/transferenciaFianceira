5f028d8d038fed85974916182522e03828e7d3a3
jrpbjr <jrpbjr@gmail.com>
2023-12-01 17:45:33 -0300

    first commit

---
+++ src/main/java/com/app/transferenciafianceira/TransferenciaFianceiraApplication.java
@@ -0,0 +1,13 @@
+package com.app.transferenciafianceira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferenciaFianceiraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransferenciaFianceiraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/transferenciafianceira/TransferenciaFianceiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.transferenciafianceira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferenciaFianceiraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
