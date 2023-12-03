89bd057bd92a8bb1608e5fc5af0e7636c64cccd0
jrpbjr <jrpbjr@gmail.com>
2023-12-03 00:51:27 -0300

    Configuração swagger versao 3.0

--- src/main/java/com/app/transferenciafianceira/TransferenciaFianceiraApplication.java
+++ src/main/java/com/app/transferenciafianceira/TransferenciaFianceiraApplication.java
@@ -2,7 +2,6 @@ package com.app.transferenciafianceira;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class TransferenciaFianceiraApplication {
 
---
+++ src/main/java/com/app/transferenciafianceira/config/SwaggerConfiguration.java
@@ -0,0 +1,29 @@
+package com.app.transferenciafianceira.config;
+
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.tags.Tag;
+import org.springframework.context.annotation.Bean;
+
+
+import java.util.Arrays;
+@Configuration
+public class SwaggerConfiguration {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Transferencia API")
+                        .version("v1")
+                        .description("REST API Test")
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://springdoc.org")
+                        )
+                );
+
+    }
+}
